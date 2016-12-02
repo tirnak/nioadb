ce62880502719b054ae9a21433c0714f3037b42c
kise0116 <w@2>
2016-12-02 18:11:27 +0300

    rolling queue

--- src/main/java/org/tirnak/nioadb/MyFirstVerticle.java
+++ src/main/java/org/tirnak/nioadb/MyFirstVerticle.java
@@ -2,6 +2,7 @@ package org.tirnak.nioadb;
 
 import io.vertx.core.*;
 import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.file.AsyncFile;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.jdbc.JDBCClient;
@@ -32,9 +33,8 @@ public class MyFirstVerticle extends AbstractVerticle {
                 if (res.succeeded()) {
                     SQLConnection connection = res.result();
                     connection.query("SELECT * FROM job", res2 -> {
-                        if (res2.succeeded()) {
-                            final List<JsonArray> results = res2.result().getResults();
-                        }
+                        List<JsonArray> results = res2.result().getResults();
+                        r.response().end(results.toString());
                         connection.close();
                     });
                 } else {
@@ -56,11 +56,13 @@ public class MyFirstVerticle extends AbstractVerticle {
                 ResultSet resultSet = jobQueryResult.result();
                 if (resultSet.getNumRows() == 0) {
                     eb.publish("device.free", deviceName);
+                    return;
                 }
                 JsonObject jobRow = resultSet.getRows().get(0);
+                JsonObject params = new JsonObject().put("ACC_ID", jobRow.getValue("ACC_ID"))
+                                                    .put("SCENARIO_ID", jobRow.getValue("SCENARIO_ID"));
                 ArrayList<String> instructions = new ArrayList<>(Arrays.asList(jobRow.getString("SCENARIO_TEXT").split(";")));
-                executeInstructions(instructions, deviceName);
-
+                executeInstructions(instructions, deviceName, params);
             };
 
             Handler<AsyncResult<SQLConnection>> getReservedJob = connectionResult -> {
@@ -79,45 +81,62 @@ public class MyFirstVerticle extends AbstractVerticle {
                 connection.close();
             };
 
-            Handler<AsyncResult<SQLConnection>> createSampleData = connectionResult -> {
-                SQLConnection connection = connectionResult.result();
-
-                List<String> batch = new ArrayList<>();
-                batch.add("CREATE TABLE IF NOT EXISTS scenario(scenario_id INTEGER, scenario_text CLOB(300))");
-                batch.add("CREATE TABLE IF NOT EXISTS job(acc_id INTEGER, " +
-                                "                  scenario_id INTEGER," +
-                                "                  state INTEGER," +
-                                "                  acquired_by VARCHAR(30))");
-                batch.add("INSERT INTO scenario (scenario_id, scenario_text) VALUES (1, 'sleep 1;sleep 2')");
-                batch.add("INSERT INTO scenario (scenario_id, scenario_text) VALUES (2, 'sleep 1;')");
-                batch.add("INSERT INTO job (acc_id, scenario_id, state, acquired_by) VALUES (1,1,0,'')");
-                batch.add("INSERT INTO job (acc_id, scenario_id, state, acquired_by) VALUES (1,2,0,'')");
-                batch.add("INSERT INTO job (acc_id, scenario_id, state, acquired_by) VALUES (2,1,0,'')");
-                batch.add("INSERT INTO job (acc_id, scenario_id, state, acquired_by) VALUES (2,2,0,'')");
-                batch.add("INSERT INTO job (acc_id, scenario_id, state, acquired_by) VALUES (3,1,0,'')");
-                batch.add("INSERT INTO job (acc_id, scenario_id, state, acquired_by) VALUES (3,2,0,'')");
-                connection.batch(batch, ignored -> jdbc.getConnection(reserveJob));
-
-                connection.close();
-            };
-
-            jdbc.getConnection(createSampleData);
+            jdbc.getConnection(reserveJob);
         });
 
         System.out.println("verticle mfv has started");
 
-        List<String> devices = Arrays.asList("qwerty", "asdf", "1234");
-        devices.forEach(device -> {
-            eb.publish("device.free", device);
-        });
+        Handler<AsyncResult<List<Integer>>> initialSendDevices = ignored -> {
+            List<String> devices = Arrays.asList("qwerty", "asdf", "1234");
+            devices.forEach(device -> {
+                eb.publish("device.free", device);
+            });
+        };
+
+        Handler<AsyncResult<SQLConnection>> createSampleData = connectionResult -> {
+            SQLConnection connection = connectionResult.result();
+
+            List<String> batch = new ArrayList<>();
+            batch.add("CREATE TABLE IF NOT EXISTS scenario(scenario_id INTEGER, scenario_text CLOB(300))");
+            batch.add("CREATE TABLE IF NOT EXISTS job(acc_id INTEGER, " +
+                    "                  scenario_id INTEGER," +
+                    "                  state INTEGER," +
+                    "                  acquired_by VARCHAR(30))");
+            batch.add("INSERT INTO scenario (scenario_id, scenario_text) VALUES (1, 'sleep 1;sleep 2')");
+            batch.add("INSERT INTO scenario (scenario_id, scenario_text) VALUES (2, 'sleep 1;')");
+            batch.add("INSERT INTO job (acc_id, scenario_id, state, acquired_by) VALUES (1,1,0,'')");
+            batch.add("INSERT INTO job (acc_id, scenario_id, state, acquired_by) VALUES (1,2,0,'')");
+            batch.add("INSERT INTO job (acc_id, scenario_id, state, acquired_by) VALUES (2,1,0,'')");
+            batch.add("INSERT INTO job (acc_id, scenario_id, state, acquired_by) VALUES (2,2,0,'')");
+            batch.add("INSERT INTO job (acc_id, scenario_id, state, acquired_by) VALUES (3,1,0,'')");
+            batch.add("INSERT INTO job (acc_id, scenario_id, state, acquired_by) VALUES (3,2,0,'')");
+            connection.batch(batch, initialSendDevices);
+            connection.close();
+        };
+
+        jdbc.getConnection(createSampleData);
 
     }
 
-    private void executeInstructions(ArrayList<String> instructions, String deviceName) {
-        if (instructions.isEmpty()) { return; }
+    private void executeInstructions(ArrayList<String> instructions, String deviceName, JsonObject params) {
+
+        Handler<AsyncResult<SQLConnection>> completeJob = connectionResult -> {
+            SQLConnection connection = connectionResult.result();
+            String query = "update job set state = 2 where " +
+                    "state = 1 and ACC_ID = ? and SCENARIO_ID = ?";
+            JsonArray completeParams = new JsonArray().add(params.getValue("ACC_ID")).add(params.getValue("SCENARIO_ID"));
+            connection.updateWithParams(query, completeParams,
+                ignored -> eb.publish("device.free", deviceName)
+            );
+            connection.close();
+        };
+
+        if (instructions.isEmpty()) {
+            jdbc.getConnection(completeJob);
+        }
 
         Handler<Future<Object>> instructionHandler = f -> {
-            Logger logger = new Logger(random.nextInt(), System.out);
+            Logger logger = new DeviceLogger(random.nextInt(), deviceName, System.out);
             try {
                 logger.log("start to execute: ");
                 for (String s : instructions.get(0).split(" ")) {
@@ -132,23 +151,37 @@ public class MyFirstVerticle extends AbstractVerticle {
                 e.printStackTrace();
             } finally {
                 instructions.remove(0);
+                if (instructions.isEmpty()) {
+                    jdbc.getConnection(completeJob);
+                }
                 f.complete(instructions);
             }
         };
 
         Handler<AsyncResult<Object>> schedulingHandler = instructionResult -> {
-            executeInstructions((ArrayList<String>) instructionResult.result(), deviceName);
+            executeInstructions((ArrayList<String>) instructionResult.result(), deviceName, params);
         };
 
         vertx.executeBlocking(instructionHandler, schedulingHandler);
     }
 
     private class Logger {
-        private int id;
-        private PrintStream ps;
+        protected int id;
+        protected PrintStream ps;
         public Logger(int id, PrintStream ps) {
             this.id = id;
             this.ps = ps;
+        }
+        public void log(String s) {
+            ps.println(id + ": " + s);
+        }
+    }
+
+    private class DeviceLogger extends Logger {
+        String deviceName;
+        public DeviceLogger(int id, String deviceName, PrintStream ps) {
+            super(id, ps);
+            this.deviceName = deviceName;
         }
         public void log(String s) {
             ps.println(id + ": " + s);
